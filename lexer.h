62c9c0be5bfee8dfb70df9afcbfcabf5029607c7
VivekRegulus <128822236+VivekRegulus@users.noreply.github.com>
2026-03-02 17:53:00 +0530

    Add student IDs and names to lexer.h
    Added student ID and names as comments.

--- lexer.h
+++ lexer.h
@@ -1,3 +1,10 @@
+/* ID:  2022B3A70404P; Name: Vivek Raj Barnwal */
+/* ID:  2022B3A70607P; Name: Abhinav Madaan */
+/* ID:  2022B3A70413P; Name: Jayant Grover */
+/* ID:  2022B3A70648P; Name: Gathvik Narayan Kolla */
+/* ID:  2022B3A70474P; Name: Raghav Mundra */
+/* ID: 2023A7PS0653P; Name: Vulli Abhinav */
+
 #ifndef LEXER_H
 #define LEXER_H
 
